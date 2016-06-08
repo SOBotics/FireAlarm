4837a1190449514d5aa75af0332b824e37f8042b
Ashish Ahuja <shivashish.ahuja@gmail.com>
2016-06-08 16:03:09 +0530

    Initial Commit

---
+++ chatbot/Privileges.h
@@ -0,0 +1,22 @@
+//
+// Privileges.h
+// chatbot
+//
+// Created on 8/06/2016
+// Copyright © 2016 Ashish Ahuja. All rights reserved.
+//
+
+#ifndef Client_h
+#define Client_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "cJSON.h"
+
+typedef struct {
+    long privUserId;
+}PrivUsers;
+
+#endif /* Privileges.h */
