fb8618e5c2da60fb78a02d0c7d9ca346044ddffd
Ashish Ahuja <shivashish.ahuja@gmail.com>
2016-06-11 09:09:23 +0530

    Update Privileges.h

--- chatbot/Privileges.h
+++ chatbot/Privileges.h
@@ -18,10 +18,11 @@
 typedef struct {
     long userId;
     char *username;
+    int privLevel;  // 1 if member, 2 if bot owner.
 }PrivUsers;
 
 unsigned int checkPrivUser (ChatBot *bot, long userID);
-PrivUsers *createPrivUsers (long userID);
+PrivUsers *createPrivUsers (long userID, char *username, int privLevel);
 unsigned userPrivCheck (ChatBot *bot, long userID);
 unsigned commandPriv (RunningCommand *commands);
 unsigned commandPrivCheck (RunningCommand *command, ChatBot *bot);
