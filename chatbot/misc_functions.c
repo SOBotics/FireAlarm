f3fcc405cdf10f24a78c30793dce5e98d942ae1f
Ashish Ahuja <shivashish.ahuja@gmail.com>
2016-06-11 18:38:07 +0530

    Added function "getUsernameByID"

--- chatbot/misc_functions.c
+++ chatbot/misc_functions.c
@@ -19,4 +19,46 @@ void lowercase (char *str)
     return;
 }
 
-/* End of file: misc_functions.h */
+char *getUsernameByID (ChatBot *bot, long userID)
+{
+    pthread_mutex_lock(&bot->room->clientLock);
+    const int maxUrlLength = 256;
+    char url[maxUrlLength];
+    url[0] = 0;
+    snprintf(url, maxUrlLength,
+             "chat.%s/rooms/pingable/%d",
+             bot->room->client->host,
+             bot->room->roomID
+             );
+    
+    CURL *curl = bot->room->client->curl;
+    checkCURL(curl_easy_setopt(curl, CURLOPT_HTTPGET, 1));
+    checkCURL(curl_easy_setopt(curl, CURLOPT_URL, url));
+    OutBuffer buffer;
+    buffer.data = NULL;
+    checkCURL(curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer));
+    
+    checkCURL(curl_easy_perform(curl));
+    cJSON *json = cJSON_Parse(buffer.data);
+    free(buffer.data);
+    
+    pthread_mutex_unlock(&bot->room->clientLock);
+    
+    const int userCount = cJSON_GetArraySize(json);
+    
+    for (int i = 0; i < userCount; i ++)
+    {
+        cJSON *item = cJSON_GetArrayItem(json, i);
+        unsigned long user_id = cJSON_GetArrayItem(item, 0)->valueint;
+        
+        if (user_id == userID)
+        {
+            const char *username = cJSON_GetArrayItem(item, 1)->valuestring;
+            cJSON_Delete (json);
+            return username;
+        }
+    }
+    
+    cJSON_Delete (json);
+    return 1;
+}
