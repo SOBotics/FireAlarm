735555573e2283ca79dc73b9bd6c14f84539a66c
Ashish Ahuja <shivashish.ahuja@gmail.com>
2016-06-25 20:18:55 +0530

    Initial Commit

---
+++ chatbot/mode_commands.h
@@ -0,0 +1,156 @@
+//
+//  chatbot
+//  mode_commands.h
+//
+//  Created by Ashish Ahuja on 25/06/2016
+//  Copyright © 2016 Ashish Ahuja (Fortunate-MAN). All rights reserved.
+//
+
+#ifndef mode_commands_h
+#define mode_commands_h
+
+void changeMode (RunningCommand *command, void *ctx)
+{
+    ChatBot *bot = ctx;
+    if (commandPrivCheck (command, bot))
+    {
+        return;
+    }
+    
+    if (command->argc == 0 || command->argc == 1)
+    {
+        postReply (bot->room, "**Usage:** `@FireAlarm mode [enable/disbale] [group name] [optional item]", command->message);
+        return;
+    }
+    
+    int check = 0;
+    
+    if (command->argc == 3)
+    {
+        check = 1;
+    }
+    
+    char *option = command->argv [0];
+    char *group = command->argv [1];
+    if (check)
+    {
+        char *item = command->argv [2];
+    }
+    
+    int enable = 0;
+    int disable = 0;
+    int report = 0;
+    int filter = 0;
+    int keywordFilter = 0;
+    int lengthFilter = 0;
+    int message;
+    
+    if (strcmp (option, "enable") == 0)
+    {
+        enable = 1;
+    }
+    else if (strcmp (option, "disable") == 0)
+    {
+        disable = 1;
+    }
+    else
+    {
+        char *message;
+        asprintf (&message, "`%s` is invalid. You can either enter `enable` or `disable`.", option);
+        postReply (bot->room, message, command->message);
+        free (message);
+        free (group);
+        free (item);
+        free (option);
+        return;
+    }
+    
+    if (strcmp (group, "report") == 0)
+    {
+        report = 1;
+    }
+    else if (strcmp (group, "filter") == 0)
+    {
+        if (!check)
+        {
+            postReply (bot->room, "If you use the group `filter`, you need to provide a third argument specifying the filter type.", command->message);
+            return;
+        }
+        
+        filter = 1;
+        
+        if (strcmp (item, "keyword") == 0)
+        {
+            keywordFilter = 1;
+        }
+        else if (strcmp (item, "length") == 0)
+        {
+            lengthFilter = 1;
+        }
+    }
+    else if (strcmp (group, "message") == 0 || strcmp (group, "messages") == 0)
+    {
+        message = 1;
+    }
+    else
+    {
+        char *message;
+        asprintf (&message, "Invalid group type %s. ", group);
+        postReply (bot->room, message, command->message);
+        free (message);
+        free (group);
+        free (item);
+        free (option);
+        return;
+    }
+    
+    if (disable)
+    {
+        if (report)
+        {
+            disableMode (bot, 0);
+        }
+        else if (keywordFilter)
+        {
+            disableMode (bot, 1);
+        }
+        else if (lengthFilter)
+        {
+            disableMode (bot, 2);
+        }
+        else if (message)
+        {
+            disableMode (bot, 3);
+        }
+    }
+    else if (enable)
+    {
+        if (report)
+        {
+            enableMode (bot, 0);
+        }
+        else if (keywordFilter)
+        {
+            enableMode (bot, 1);
+        }
+        else if (lengthFilter)
+        {
+            enableMode (bot, 2);
+        }
+        else if (message)
+        {
+            enableMode (bot, 3);
+        }
+    }
+    
+    char *message;
+    asprintf (&message, "`%s` has been %sd successfully.", group, option);
+    postReply (bot->room, message, command->message);
+    free (message);
+    free (group);
+    free (item);
+    free (option);
+    return;
+}
+
+#endif /* mode_commands.h */
