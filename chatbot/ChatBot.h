3f54253df3df1bd552a749e0a61475a720de22cf
Ashish Ahuja <shivashish.ahuja@gmail.com>
2016-06-27 17:06:05 +0530

    Edited structure "ChatBot"

--- chatbot/ChatBot.h
+++ chatbot/ChatBot.h
@@ -54,6 +54,7 @@ typedef struct _ChatBot {
     unsigned numOfPrivUsers;
     PrivRequest **privRequests;
     unsigned totalPrivRequests;
+    Modes *modes;
     Report *latestReports[REPORT_MEMORY];   //index 0 is the most recent report, 1 is the second most, etc.
     int reportsWaiting; //The amount of reports that have not yet been assigned a message ID.
     int reportsUntilAnalysis;   //The number of reports left until the bot analyzes them to auto-generate filters.
