270927279ed113357f05b4d65008ee92ef83b52f
Ashish Ahuja <shivashish.ahuja@gmail.com>
2016-07-05 08:20:40 +0530

    Added function reference

--- chatbot/ChatRoom.h
+++ chatbot/ChatRoom.h
@@ -34,6 +34,7 @@ void enterChatRoom(ChatRoom *room);
 ChatMessage **processChatRoomEvents(ChatRoom *room);    //Returns a NULL-terminated list of chat messages.
                                                         //The list must be freed when done.
 ChatUser *getUserByID(ChatRoom *r, unsigned long id);
+char *getUsernameByID (ChatBot *bot, long userID);
 void postMessage(ChatRoom *r, const char *text);
 void postReply(ChatRoom *r, const char *text, ChatMessage *message);
 void leaveRoom(ChatRoom *r);
