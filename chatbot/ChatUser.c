4ceb91b6a419e6661033b854b267ab47c81f2543
Ashish Ahuja <shivashish.ahuja@gmail.com>
2016-07-17 11:47:10 +0530

    Moved function

--- chatbot/ChatUser.c
+++ chatbot/ChatUser.c
@@ -27,3 +27,49 @@ void deleteUser(ChatUser *u) {
     free(u->name);
     free(u);
 }
+
+char *getUsernameByID (ChatBot *bot, long userID)
+{
+    pthread_mutex_lock(&bot->room->clientLock);
+    const int maxUrlLength = 256;
+    char url[maxUrlLength];
+    url[0] = 0;
+    snprintf(url, maxUrlLength,
+             "chat.%s/rooms/pingable/%d",
+             bot->room->client->host,
+             bot->room->roomID
+             );
+    
+    CURL *curl = bot->room->client->curl;
+    checkCURL(curl_easy_setopt(curl, CURLOPT_HTTPGET, 1));
+    checkCURL(curl_easy_setopt(curl, CURLOPT_URL, url));
+    OutBuffer buffer;
+    buffer.data = NULL;
+    checkCURL(curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer));
+    
+    checkCURL(curl_easy_perform(curl));
+    cJSON *json = cJSON_Parse(buffer.data);
+    free(buffer.data);
+    
+    pthread_mutex_unlock(&bot->room->clientLock);
+    
+    const int userCount = cJSON_GetArraySize(json);
+    
+    for (int i = 0; i < userCount; i ++)
+    {
+        cJSON *item = cJSON_GetArrayItem(json, i);
+        unsigned long user_id = cJSON_GetArrayItem(item, 0)->valueint;
+        
+        if (user_id == userID)
+        {
+            const char *username = cJSON_GetArrayItem(item, 1)->valuestring;
+            char *result = malloc(strlen(username) + 1);
+            strcpy(result, username);
+            cJSON_Delete (json);
+            return result;
+        }
+    }
+    
+    cJSON_Delete (json);
+    return NULL;
+}
