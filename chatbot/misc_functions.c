a1ba3c59cffc924ec9a430008fce08320a8b1bce
Ashish Ahuja <shivashish.ahuja@gmail.com>
2016-07-30 12:55:14 +0530

    Tag Filter

--- chatbot/misc_functions.c
+++ chatbot/misc_functions.c
@@ -38,3 +38,188 @@ void removeSpaces(char* source)
   return;
 }
 
+int isTagProgrammingRelated (char *tag)
+{
+    char *progTags[] = {
+        ".net",
+        "accessibility-api",
+        "actionscript",
+        "actionscript-3",
+        "ajax",
+        "alljoyn",
+        "applescript",
+        "android-ndk",
+        "android-sdk",
+        "android-sdk-2.1",
+        "android-sdk-2.2",
+        "android-sdk-2.3",
+        "anrdoid-sdk-tools",
+        "android-studio",
+        "angularjs",
+        "asp.net",
+        "asp.net5",
+        "asp.net-mvc",
+        "asp.net-mvc-3",
+        "asp.net-mvc-4",
+        "asp.net-mvc-5",
+        "asp.net-mvc6",
+        "awk",
+        "bash",
+        "c",
+        "c#",
+        "c++",
+        "c++11",
+        "cakephp",
+        "cakephp-1.0",
+        "cakephp-1.3",
+        "cakephp-2.0",
+        "cakephp-2.1",
+        "cakephp-2.3",
+        "cakephp-3.0",
+        "clisp",
+        "clojure",
+        "cocoa",
+        "cocoa-touch",
+        "cocos2d",
+        "cocos2d-iphone",
+        "cocos2d-x",
+        "codeigniter",
+        "common-lisp",
+        "cordova",
+        "core-text",
+        "css",
+        "data-structures",
+        "delphi",
+        "django",
+        "dom",
+        "elisp",
+        "excel-vba",
+        "expect",
+        "git",
+        "grails",
+        "hadoop",
+        "haskell",
+        "html",
+        "html5",
+        "jasper-reports",
+        "java",
+        "java-ee",
+        "javascript",
+        "jpa",
+        "jsf",
+        "json",
+        "jsp",
+        "jquery",
+        "jquery-ui",
+        "laravel",
+        "lisp",
+        "magento",
+        "matlab",
+        "maven",
+        "mercurial",
+        "mongodb",
+        "msbuild",
+        "mybatis",
+        "mysql",
+        "netbeans",
+        "node.js",
+        "numpy",
+        "objective-c",
+        "oop",
+        "opencv",
+        "opengl",
+        "oracle",
+        "pandas",
+        "perl",
+        "powershell",
+        "php",
+        "prolog",
+        "python",
+        "python-2.7",
+        "python-2.x",
+        "python-3.x",
+        "qt",
+        "r",
+        "racket",
+        "regex",
+        "ruby",
+        "ruby-on-rails",
+        "ruby-on-rails-3",
+        "ruby-on-rails-4",
+        "sapui",
+        "sapui5",
+        "scala",
+        "scheme",
+        "sed",
+        "selenium",
+        "sh",
+        "shell",
+        "sockets",
+        "spring-mybatis",
+        "spring-mvc",
+        "sql",
+        "sql-server",
+        "sql-server-2008",
+        "sqlite",
+        "swift",
+        "symfony1",
+        "symfony2",
+        "symfony3",
+        "tcl",
+        "tsql",
+        "vb.net",
+        "vb6",
+        "vba",
+        "vbscript",
+        "verilog",
+        "visual-studio",
+        "visual-studio-2010",
+        "vscode",
+        "winapi",
+        "wcf",
+        "xaml",
+        "xcode",
+        "xml",
+        "xslt"
+    }
+    
+    for (int i = 0; i < 139; i ++)
+    {
+        if (strcasestr (tag, progTags [i]) == tag)
+        {
+            return 1;
+        }
+    }
+    
+    return 0;
+}
+
+int postHasTags (Post *post, char *tag)
+{
+    char **tags = getTagsByID (post->postID);
+    
+    for (int i = 0; i < 5; i ++)
+    {
+        if (strcmp (tags [i], tag) == 0)
+        {
+            return 1;
+        }
+    }
+    
+    return 0;
+}
+
+unsigned isTagInFilter (ChatBot *bot, char *tag)
+{
+    Filter **filters = bot->filters;
+    
+    for (int i = 0; i < bot->totalFilters; i ++)
+    {
+        if (strcmp (filters [i]->filter, tag) == 0)
+        {
+            return 1;
+        }
+    }
+    
+    return 0;
+}
