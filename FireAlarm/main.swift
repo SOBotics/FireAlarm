8982a2ce6894d452c2757869e7e4eee68c7e1c6e
Jonathan Keller <jkeller234@icloud.com>
2016-08-27 12:01:46 -0700

    Initial Commit

---
+++ FireAlarm/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FireAlarm
+//
+//  Created by Jonathan Keller on 8/27/16.
+//  Copyright © 2016 NobodyNada. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
