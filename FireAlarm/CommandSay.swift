df9907f9c2b6d53e74c19c4dbe2e9d114fc97727
Jonathan Keller <jkeller234@icloud.com>
2016-10-01 13:20:25 -0700

    moved tag to the beginning of the report

---
+++ FireAlarm/CommandSay.swift
@@ -0,0 +1,19 @@
+//
+//  CommandSay.swift
+//  FireAlarm
+//
+//  Created by Jonathan Keller on 10/1/16.
+//  Copyright © 2016 NobodyNada. All rights reserved.
+//
+
+import Foundation
+
+class CommandSay: Command {
+	override class func usage() -> [String] {
+		return ["say ..."]
+	}
+	
+	override func run() throws {
+		bot.room.postMessage(arguments.joined(separator: " "))
+	}
+}
