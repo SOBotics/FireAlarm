86629d233909d7fe3788b6592c92aa358c293adc
Jonathan Keller <jkeller234@icloud.com>
2016-10-02 20:42:56 -0700

    fixed warnings

--- FireAlarm/WebSocket.swift
+++ FireAlarm/WebSocket.swift
@@ -1518,8 +1518,8 @@ private class InnerWebSocket: Hashable {
             }
         }
 		let inputStream = InputStream(fileAtPath: "/dev/urandom")!
-		var buffer = UnsafeMutablePointer<UInt32>.allocate(capacity: 1)
-		buffer.withMemoryRebound(to: UInt8.self, capacity: 4) {
+		let buffer = UnsafeMutablePointer<UInt32>.allocate(capacity: 1)
+		let _ = buffer.withMemoryRebound(to: UInt8.self, capacity: 4) {
 			inputStream.read($0, maxLength: 4)
 		}
 		let r = buffer.pointee
