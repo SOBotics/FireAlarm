1a2fcf5b4bb95215e5de0d6a9b0f9fd9fe331ec8
Fortunate-MAN <shivashish.ahuja@gmail.com>
2016-10-22 12:23:34 +0530

    Uncluttered main.c

---
+++ chatbot/LoadData.h
@@ -0,0 +1,38 @@
+//
+//  LoadData.c
+//  chatbot
+//
+//  Created by Ashish Ahuja on 10/22/16.
+//  Copyright © 2016 Fortunate-MAN (Ashish Ahuja). All rights reserved.
+//
+
+#ifndef LoadData_h
+#define LoadData_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "cJSON.h"
+#include "ChatBot.h"
+#include "Privileges.h"
+#include "Notifications.h"
+
+Log **loadLogs ();
+void saveLogs (Log **logs, unsigned totalLogs);
+
+Filter **loadFilters ();
+void saveFilters ();
+
+PrivRequest **loadPrivRequests ();
+void savePrivRequests ();
+
+Notify **loadNotifications ();
+void saveNotifications ();
+
+PrivUser **loadPrivUsers ();
+void savePrivUsers ();
+
+cJSON *loadReports();
+void saveReports ();
+
+#endif // LoadData_h
