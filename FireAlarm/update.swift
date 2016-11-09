342a4f89536f99eae49dcdfa2cd0c6b9dd6758a2
Jonathan Keller <jkeller234@icloud.com>
2016-11-09 10:06:31 -0800

    updated machine info string

--- FireAlarm/update.swift
+++ FireAlarm/update.swift
@@ -22,11 +22,16 @@ func launchProcess(path: String, arguments: [String]) -> Process {
 
 func installUpdate() -> Bool {
 	do {
+		#if os(Linux)
+			let compile = "swiftc -sdk /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.12.sdk " +
+			"-target x86_64-macosx10.11 -lz -lc++ -o ../FireAlarm FireAlarm/*.swift "
+		#else
+			let compile = "swiftc -l:libz.a -o ../FireAlarm FireAlarm/*.swift "
+		#endif
 		let updateScript = "rm -rf update;pushd .;" +
 			"(git clone -b swift \"https://github.com/NobodyNada/FireAlarm.git\" update && " +
 			"cd update && " +
-			"swiftc -sdk /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.12.sdk " +
-			"-target x86_64-macosx10.11 -lz -lc++ -o ../FireAlarm FireAlarm/*.swift && " +
+			compile + "&& " +
 			"git log --format='oneline' -n 1 > ../version-new.txt && " +
 			"cd .. && " +
 			"rm -rf update) || " +
