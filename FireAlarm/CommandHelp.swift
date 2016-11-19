73489dd2e4bc66f116fce879b9f87c70c5c62726
Jonathan Keller <jkeller234@icloud.com>
2016-11-19 14:52:15 -0800

    improved help command

--- FireAlarm/CommandHelp.swift
+++ FireAlarm/CommandHelp.swift
@@ -14,7 +14,6 @@ class CommandHelp: Command {
     }
     
     override func run() throws {
-        bot.room.postReply("I'm  [FireAlarm](https://github.com/NobodyNada/chatbot), a bot which detects questions that need closing. [My command list is available here](https://github.com/NobodyNada/chatbot/wiki/Commands).", to: message)
-        bot.room.postMessage("(Note: Most of those commands have not yet been implemented in this Swift rewrite.)")
+        bot.room.postReply("I'm  [FireAlarm-Swift](\(githubLink)), a bot which detects questions that need closing. [My command list is available here](https://github.com/NobodyNada/FireAlarm/wiki/CommandsSwift).", to: message)
     }
 }
