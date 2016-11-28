8b751d44cda3edec7b7c20835e292014c3fbc969
Jonathan Keller <jkeller234@icloud.com>
2016-11-28 15:26:18 -0800

    updated version

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
 	name: "FireAlarm",
 	dependencies: [
-		.Package(url: "git://github.com/NobodyNada/SwiftChatSE", majorVersion: 0)
+		.Package(url: "git://github.com/NobodyNada/SwiftChatSE", majorVersion: 1)
 	]
 )
