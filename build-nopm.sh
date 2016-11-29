61bd266484475d8b4764005f7df91543ab8b998f
NobodyNada <jkeller234@icloud.com>
2016-11-29 15:32:15 -0800

    stop building on error

--- build-nopm.sh
+++ build-nopm.sh
@@ -3,12 +3,12 @@
 #download and build SwiftChatSE
 echo "Building SwiftChatSE..."
 [[ -d SwiftChatSE ]] && rm -rf SwiftChatSE
-git clone git://github.com/NobodyNada/SwiftChatSE
-pushd SwiftChatSE
-build-nopm.sh
+git clone git://github.com/NobodyNada/SwiftChatSE || exit 1
+pushd SwiftChatSE || exit 1
+build-nopm.sh || (popd; exit 1)
 popd
 
 
 #build FireAlarm
-echo "Building FireAlarm..."
-swiftc Sources/*.swift -L/usr/local/lib -ISwiftChatSE -LSwiftChatSE -lSwiftChatSE -o FireAlarm
+echo "Building FireAlarm..." || exit 1
+swiftc Sources/*.swift -L/usr/local/lib -ISwiftChatSE -LSwiftChatSE -lSwiftChatSE -o FireAlarm || exit 1
