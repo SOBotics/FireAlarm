873aed32ab71c8f61511e8289c67c96395e8cc5e
Fortunate-MAN <shivashish.ahuja@gmail.com>
2016-12-03 11:07:21 +0530

    some shell scripts

---
+++ shellscripts/copyfiles.sh
@@ -0,0 +1,6 @@
+cp ~/.chatbot/reports.json firealarm.files/reports.json
+cp ~/.chatbot/api.json firealarm.files/api.json
+cp ~/.chatbot/filters.json firealarm.files/filters.json
+cp ~/.chatbot/notifications.json firealarm.files/notifications.json
+cp ~/.chatbot/privRequest.json firealarm.files/privRequest.json
+cp ~/.chatbot/privUsers.json firealarm.files/privUsers.json
