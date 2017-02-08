38944b87d37e945017971f066bccc79d05ec194c
NobodyNada <jkeller234@icloud.com>
2017-02-08 13:16:57 -0800

    run in SOCVR

--- Sources/main.swift
+++ Sources/main.swift
@@ -187,7 +187,7 @@ func main() throws {
 	else {
 		rooms = [
 			ChatRoom(client: client, roomID: 111347), //SOBotics
-			ChatRoom(client: client, roomID: 68414),  //SOCVR Testing Facility
+			ChatRoom(client: client, roomID: 41570),  //SO Close Vote Reviewers
 		]
 		
 		development = false
