46edcbc1c1d074e77e76a33f91f306145327f7a8
Jonathan Keller <jkeller234@icloud.com>
2017-02-12 18:05:54 -0800

    fixed compile error

--- Sources/Filter.swift
+++ Sources/Filter.swift
@@ -377,7 +377,7 @@ class Filter {
 		}
 		else if let q = post as? Question {
 			if (post.id ?? 1) % 10000 == 0 && q.last_edit_date == nil {
-				rooms.first.postMessage("[ [\(botName)](\(githubLink)) ] " +
+				rooms.first!.postMessage("[ [\(botName)](\(githubLink)) ] " +
 					"[tag:\(tags(for: post).first ?? "tagless")] Potentially bad question: " +
 					"[\(post.title ?? "<no title>")](//youtube.com/watch?v=dQw4w9WgXcQ)"
 					)
