b8345b9181353563632c52e6177004febceb2382
Jonathan Keller <jkeller234@icloud.com>
2017-09-02 17:54:14 -0700

    Fix CryptoSwift version

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ let package = Package(
     dependencies: [
         .Package(url: "git://github.com/SOBotics/SwiftChatSE",      majorVersion: 4),
         .Package(url: "git://github.com/SOBotics/SwiftStack",       majorVersion: 0),
-        .Package(url: "git://github.com/krzyzanowskim/CryptoSwift", majorVersion: 0)
+        .Package(url: "git://github.com/krzyzanowskim/CryptoSwift", majorVersion: 0, minorVersion: 6)
     ]
 )
