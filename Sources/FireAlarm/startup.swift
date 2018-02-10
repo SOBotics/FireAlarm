5bb20d7900e3b23d9581029ff46b3dee3463871e
Jonathan Keller <jkeller234@icloud.com>
2018-02-09 22:07:13 -0800

    Disable train wrecker

--- Sources/FireAlarm/startup.swift
+++ Sources/FireAlarm/startup.swift
@@ -232,9 +232,7 @@ func main() throws {
     
     listener = ChatListener(commands: commands)
     listener.onShutdown { shutDown(reason: $0, rooms: rooms) }
-    rooms.forEach { room in
-        let trainWrecker = TrainWrecker(room: room)
-        room.onMessage { message, isEdit in
+    rooms.forEach { room in        room.onMessage { message, isEdit in
             let content = message.content.lowercased()
             if (content == "@bots alive") {
                 do {
@@ -247,7 +245,6 @@ func main() throws {
                 room.postMessage("[🚃](https://www.youtube.com/watch?v=dQw4w9WgXcQ)")
             }
             
-            if !isEdit { trainWrecker.process(message: message) }
             listener.processMessage(room, message: message, isEdit: isEdit)
         }
     }
