b1e8a36d9dfe122d18eee4785607adbe797913f5
Fortunate-MAN <shivashish.ahuja@gmail.com>
2018-03-09 19:03:33 +0530

    Fix Build

--- Sources/FireAlarm/startup.swift
+++ Sources/FireAlarm/startup.swift
@@ -232,9 +232,10 @@ func main() throws {
     
     listener = ChatListener(commands: commands)
     listener.onShutdown { shutDown(reason: $0, rooms: rooms) }
-    rooms.forEach { room in    
+    rooms.forEach { room in
+			let trainWrecker: TrainWrecker?
 			if(room.roomID == 123602 || room.roomID == 111347) {
-				let trainWrecker = TrainWrecker(room: room)
+				trainWrecker = TrainWrecker(room: room)
 			}
 			room.onMessage { message, isEdit in
 			
@@ -250,8 +251,8 @@ func main() throws {
                 room.postMessage("[🚃](https://www.youtube.com/watch?v=dQw4w9WgXcQ)")
             }
             
-			if message.room.roomID == 111347 || message.room.roomID == 123602 {
-				if !isEdit { trainWrecker.process(message: message) }
+			if (message.room.roomID == 111347 || message.room.roomID == 123602) && let wrecker = trainWrecker {
+				if !isEdit { wrecker.process(message: message) }
 			}
 							
             listener.processMessage(room, message: message, isEdit: isEdit)
