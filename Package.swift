42d47c83d6a465e62a1ce2dcfd0fb6573abb605f
Jonathan Keller <jkeller234@icloud.com>
2019-01-15 18:10:41 -0800

    update SwiftChatSE, fix "tests"

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "FireAlarm",
     dependencies: [
-        .package(url: "git://github.com/SOBotics/SwiftChatSE",      from: "5.0.7"),
+        .package(url: "git://github.com/SOBotics/SwiftChatSE",      from: "5.1.0"),
         .package(url: "git://github.com/SOBotics/SwiftStack",       from: "0.5.0"),
         .package(url: "git://github.com/krzyzanowskim/CryptoSwift", from: "0.9.0")
     ],
